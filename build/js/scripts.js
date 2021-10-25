c48be67a3cb056ed05c2db5cadbce95c198ad882
Harry Pujols <harrypujols@gmail.com>
2021-10-25 12:23:52 -0400

    adding button for process

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -201,17 +201,29 @@ __webpack_require__.r(__webpack_exports__);
   constructor ( element ) {
     this.element = element
     this.input = document.getElementById('birthdate')
+    this.button = document.getElementById('getsign')
   }
 
   birthdate() {
+    var result
+    this.input.valueAsDate = new Date();
+
     this.input.addEventListener('change', ( event ) => {
+      this.input.value = event.target.value;
       console.log(this.input.value);
-      return this.input.value;
+    })
+  }
+
+  getsign() {
+    this.button.addEventListener('click', ( event ) => {
+      console.log(this.input.value);
+      console.log('clicked');
     })
   }
 
   init ( ) {
     this.birthdate();
+    this.getsign();
   }
 });
 
--- dev/js/components/birthdate.js
+++ dev/js/components/birthdate.js
@@ -2,16 +2,28 @@ export default class {
   constructor ( element ) {
     this.element = element
     this.input = document.getElementById('birthdate')
+    this.button = document.getElementById('getsign')
   }
 
   birthdate() {
+    var result
+    this.input.valueAsDate = new Date();
+
     this.input.addEventListener('change', ( event ) => {
+      this.input.value = event.target.value;
       console.log(this.input.value);
-      return this.input.value;
+    })
+  }
+
+  getsign() {
+    this.button.addEventListener('click', ( event ) => {
+      console.log(this.input.value);
+      console.log('clicked');
     })
   }
 
   init ( ) {
     this.birthdate();
+    this.getsign();
   }
 }
