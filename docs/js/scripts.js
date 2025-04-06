0a17009e9441aac719759915a48fb5c9a48da517
Harry Pujols <harrypujols@gmail.com>
2025-04-06 16:38:23 +0200

    removing old svg

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -78,17 +78,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (class {
-  constructor ( element, APP ) {
-    this.element = element
-    this.form = document.getElementById('alchemy-form')
-    this.input = document.getElementById('input')
-    this.button = document.getElementById('submit')
-    this.template = document.getElementById('template')
-    this.placeholder = document.getElementById('placeholder')
-    this.svg = document.getElementById('alchemy-wheel')
-    this.reset = document.getElementById('reset')
-    this.data = APP.data.signs
-    this.render = APP.methods.render
+  constructor(element, APP) {
+    this.element = element;
+    this.form = document.getElementById("alchemy-form");
+    this.input = document.getElementById("input");
+    this.button = document.getElementById("submit");
+    this.template = document.getElementById("template");
+    this.placeholder = document.getElementById("placeholder");
+    // this.svg = document.getElementById('alchemy-wheel')
+    this.reset = document.getElementById("reset");
+    this.data = APP.data.signs;
+    this.render = APP.methods.render;
   }
 
   birthdate() {
@@ -96,7 +96,13 @@ __webpack_require__.r(__webpack_exports__);
   }
 
   sumDigits(numbers) {
-    return numbers.toString().split('').map(Number).reduce(function (a, b) {return a + b;}, 0);
+    return numbers
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce(function (a, b) {
+        return a + b;
+      }, 0);
   }
 
   getResults(numbers) {
@@ -111,41 +117,44 @@ __webpack_require__.r(__webpack_exports__);
   }
 
   getsign() {
-    this.button.addEventListener('click', ( event ) => {
-      let info =  this.input.value;
-      let date = new Date(info.replace(/-/g, '/')).toDateString();
-      let digits = ('' + info).split('').filter(v => v !== '-').map((i) => Number(i));
+    this.button.addEventListener("click", (event) => {
+      let info = this.input.value;
+      let date = new Date(info.replace(/-/g, "/")).toDateString();
+      let digits = ("" + info)
+        .split("")
+        .filter((v) => v !== "-")
+        .map((i) => Number(i));
       let calculation = digits.reduce((a, b) => a + b, 0);
       let result = this.getResults(calculation);
-    
+
       if (result > 9) {
         result = this.getResults(result);
       }
 
       let content = this.data[result];
-      content['date'] = date;
+      content["date"] = date;
 
       this.placeholder.innerHTML = this.render({
-        data: content, 
-        template: this.template.innerHTML
+        data: content,
+        template: this.template.innerHTML,
       });
 
-      this.svg.classList = '';
-      this.svg.classList.add(content['element'], 'alchemy-wheel');
-      this.form.classList.add('hidden');
-      this.reset.classList.remove('hidden');
-    })
+      // this.svg.classList = "";
+      // this.svg.classList.add(content['element'], 'alchemy-wheel');
+      this.form.classList.add("hidden");
+      this.reset.classList.remove("hidden");
+    });
   }
 
   resetForm() {
-    this.reset.addEventListener('click', ( event ) => {
+    this.reset.addEventListener("click", (event) => {
       this.placeholder.innerHTML = "";
-      this.form.classList.remove('hidden');
-      this.reset.classList.add('hidden');
-    })
+      this.form.classList.remove("hidden");
+      this.reset.classList.add("hidden");
+    });
   }
 
-  init ( ) {
+  init() {
     this.birthdate();
     this.getsign();
     this.resetForm();
