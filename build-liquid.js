e015664b10c2039d0a287fadc860e87f528147d3
Harry Pujols <harrypujols@gmail.com>
2025-06-30 10:55:46 -0400

    goodbye handlebars, hello liquid

--- build-liquid.js
+++ build-liquid.js
@@ -1,11 +1,16 @@
 const fs = require("fs");
 const path = require("path");
-const handlebars = require("handlebars");
+const { Liquid } = require("liquidjs");
 const yaml = require("js-yaml");
 
 const dataDir = path.join(__dirname, "dev/data");
 const templateDir = path.join(__dirname, "dev/templates");
 const outputDir = path.join(__dirname, "dev/html");
+
+const engine = new Liquid({
+  root: templateDir,
+  extname: ".liquid",
+});
 
 fs.readdirSync(dataDir).forEach((file) => {
   let base, dataPath, templatePath, outputPath, data;
@@ -13,13 +18,13 @@ fs.readdirSync(dataDir).forEach((file) => {
   if (file.endsWith(".json")) {
     base = path.basename(file, ".json");
     dataPath = path.join(dataDir, file);
-    templatePath = path.join(templateDir, `${base}.hbs`);
+    templatePath = path.join(templateDir, `${base}.liquid`);
     outputPath = path.join(outputDir, `${base}.html`);
     data = JSON.parse(fs.readFileSync(dataPath, "utf8"));
   } else if (file.endsWith(".yaml") || file.endsWith(".yml")) {
     base = path.basename(file, path.extname(file));
     dataPath = path.join(dataDir, file);
-    templatePath = path.join(templateDir, `${base}.hbs`);
+    templatePath = path.join(templateDir, `${base}.liquid`);
     outputPath = path.join(outputDir, `${base}.html`);
     data = yaml.load(fs.readFileSync(dataPath, "utf8"));
   } else {
@@ -28,10 +33,15 @@ fs.readdirSync(dataDir).forEach((file) => {
 
   if (fs.existsSync(templatePath)) {
     const template = fs.readFileSync(templatePath, "utf8");
-    const compiled = handlebars.compile(template);
-    const output = compiled(data);
-    fs.writeFileSync(outputPath, output, "utf8");
-    console.log(`Rendered ${outputPath}`);
+    engine
+      .parseAndRender(template, data)
+      .then((output) => {
+        fs.writeFileSync(outputPath, output, "utf8");
+        console.log(`Rendered ${outputPath}`);
+      })
+      .catch((err) => {
+        console.error(`Error rendering ${base}:`, err);
+      });
   } else {
     console.warn(`No template found for ${base}, skipping.`);
   }
